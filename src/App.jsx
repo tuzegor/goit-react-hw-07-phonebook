6668a61eee788f2aca1beb7554b858c19d0e33df
Yehor Tuz <tuzegor@gmail.com>
2022-01-25 22:01:45 +0200

    rename jsx components

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-// import { useEffect } from 'react';
+import React from 'react';
 import ContactForm from './components/ContactForm/ContactForm';
 import Filter from './components/Filter/Filter';
 import ContactList from './components/ContactList/ContactList';
